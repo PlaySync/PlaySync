eabc758d5afbb54c9ade01930e1603c639c2ece2
captainlogan1 <loob7212@colorado.edu>
2021-04-08 19:19:18 -0600

    minor fixes

--- code/flask/includes/blueprint.py
+++ code/flask/includes/blueprint.py
@@ -268,7 +268,7 @@ def profile():
 def youtubeAuth():
     auth_body = request.form['auth_body']
     user = valid_user(request.cookies.get('user'))
-    addauth('user', auth_body)
+    addauth(user, auth_body)
     return redirect('./profile')
 
 
