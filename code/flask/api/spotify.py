bbc14fc8c91ed64b19acfc6b0e5540c3e7004aed
captainlogan1 <loob7212@colorado.edu>
2021-04-10 16:24:34 -0600

    spotify auth testing

--- code/flask/api/spotify.py
+++ code/flask/api/spotify.py
@@ -28,10 +28,10 @@ def get_spotify():
         cache_handler=cache_handler, 
         show_dialog=True)
     
-    # if request.args.get("code"):
-    #     #Being redirected from Spotify auth page
-    #     auth_manager.get_access_token(request.args.get("code"))
-    #     return redirect('/profile')
+    if request.args.get("code"):
+        #Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/profile')
     
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         #Display sign in link when no token
@@ -39,7 +39,7 @@ def get_spotify():
         return redirect(auth_url)
 
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return spotify
+    return None
 
 # def spotify_auth():
 #     if not session.get('uuid'):
