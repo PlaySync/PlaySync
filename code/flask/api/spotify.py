dd2cfdc27a97bd4e2c168f7f6f3dea43db073152
captainlogan1 <loob7212@colorado.edu>
2021-04-10 23:23:44 -0600

    syntax

--- code/flask/api/spotify.py
+++ code/flask/api/spotify.py
@@ -36,6 +36,10 @@ def get_spotify():
     return None
 
 def spotify_callback():
+    if not session.get('uuid'):
+        #Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope='playlist-read-private playlist-modify-private',
     cache_handler=cache_handler, 
     client_id='ae468ff1f96549b28044be8d0419677d',
