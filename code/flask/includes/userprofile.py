41ca69b3643b62d14a6957478f15c046d7639bee
captainlogan1 <loob7212@colorado.edu>
2021-04-12 18:57:28 -0600

    indentation error

--- code/flask/includes/userprofile.py
+++ code/flask/includes/userprofile.py
@@ -1,27 +1,27 @@
 from includes.psql_conn import *
 
 def get_email(user):
-	return psql_get_email(user)
+    return psql_get_email(user)
 
 def add_auth(user, auth_body):
-	uid = psql_get_uid(user)
-	psql_write_auth(uid, 'ytmusic', auth_body)
-	return "added"
+    uid = psql_get_uid(user)
+    psql_write_auth(uid, 'ytmusic', auth_body)
+    return "added"
 
 def get_auth(user):
-	uid = psql_get_uid(user)
-	auth_body = psql_check_auth(uid, 'ytmusic')
-	return auth_body
+    uid = psql_get_uid(user)
+    auth_body = psql_check_auth(uid, 'ytmusic')
+    return auth_body
 
 def update_usr_email(user, email):
-	uid = psql_get_uid(user)
-	psql_write_email(email, uid)
-	return "added"
+    uid = psql_get_uid(user)
+    psql_write_email(email, uid)
+    return "added"
 
 def add_spotify_auth(user):
-	uid = psql_get_uid(user)
-	psql_write_auth(uid, 'spotify', 'Authorized')
-	return "added"
+    uid = psql_get_uid(user)
+    psql_write_auth(uid, 'spotify', 'Authorized')
+    return "added"
 
 def remove_spotify_auth(user):
     uid = psql_get_uid(user)
